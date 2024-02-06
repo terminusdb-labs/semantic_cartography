7d71c3554d850cf9263e52268df693159b2b1dc4
Gavin Mendel-Gleason <gavin@terminusdb.com>
2024-02-06 15:12:04 +0100

    Adding hierarchy match tester

---
+++ hierarchies/test_hierarchies.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+from dotenv import load_dotenv
+from openai import OpenAI
+import csv
+import os
+import pickle
+
+IRI_VALUE = {}
+IRI_ID = {}
+VECTOR_DATABASE = []
+OPENAI_KEY = None
+CLIENT = None
+
+load_dotenv()
+OPENAI_KEY = os.environ.get("OPENAI_KEY")
+CLIENT = OpenAI(
+    # defaults to os.environ.get("OPENAI_API_KEY")
+    api_key=OPENAI_KEY,
+)
+
+def dot(v1,v2):
+    return sum([x*y for x,y in zip(v1,v2)])
+
+def distance(v1,v2):
+   return (dot(v1,v2)-1.0) / -2.0
+
+def compare_iris(iri1,iri2):
+    return distance(VECTOR_DATABASE[IRI_ID[iri1]],
+                    VECTOR_DATABASE[IRI_ID[iri2]])
+
+def get_embeddings(texts, model="text-embedding-ada-002"):
+   texts = [text.replace("\n", " ") for text in texts]
+   response = CLIENT.embeddings.create(input=texts, model=model)
+   embeddings = [e.embedding for e in response.data]
+   return embeddings
+
+def register(row):
+    [iri1, value1, iri2, value2, bad, maybe] = row
+    if not iri1 in IRI_VALUE:
+        IRI_VALUE[iri1] = value1
+        [v1] = get_embeddings([value1])
+        id1 = len(VECTOR_DATABASE)
+        VECTOR_DATABASE.append(v1)
+    if not iri2 in IRI_VALUE:
+        IRI_VALUE[iri2] = value2
+        [v2] = get_embeddings([value2])
+        id2 = len(VECTOR_DATABASE)
+        VECTOR_DATABASE.append(v2)
+
+def load_database():
+    with open('vector.database', 'r') as vd:
+        d = pickle.load(vd)
+        IRI_VALUE = d['IRI_VALUE']
+        IRI_ID = d['IRI_ID']
+        VECTOR_DATABASE = d['VECTOR_DATABASE']
+
+
+def save_database():
+    with open('vector.database', 'w') as vd:
+        pickle.dump({
+            'IRI_VALUE': IRI_VALUE,
+            'IRI_ID': IRI_ID,
+            'VECTOR_DATABASE': VECTOR_DATABASE
+        }, vd)
+
+if __name__ == "__main__":
+    if os.path.exists('vector.database'):
+        load_database()
+    else:
+        with open('lib_places.csv', 'r') as fh:
+            reader = csv.reader(fh)
+            next(reader) # remove the header
+            for row in reader:
+                register(row)
+        save_database()
+
+    threshold = 0.03
+    with open('lib_places.csv', 'r') as fh:
+        reader = csv.reader(fh)
+        for row in reader:
+            [iri1, value1, iri2, value2, bad, maybe] = row
+            dist = compare_iris(iri1, iri2)
+            if dist > threshold:
+                print(f"{value1} not closely related to {value2}")
